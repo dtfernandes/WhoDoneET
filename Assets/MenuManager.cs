919407381b71832cf22729d469a459d20aeb83bf
unknown <csi@ULHT-07-PC12.ulht.gl.pt>
2023-08-08 09:30:48 +0100

    Prepare for bake lights

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -7,12 +7,26 @@ public class MenuManager : MonoBehaviour
     [SerializeField]
     private GameObject _log;
 
+    private bool _previousStopped;
+
     public void OnMenu()
     {
+        GameSettings _gameSettings = GameSettings.Instance;
+       
+        if (_gameSettings.isWorldStopped && !_previousStopped)
+        {
+            _previousStopped = true;
+        }
+
+        if (_previousStopped)
+        {
+            _previousStopped = false;
+        }
+
         bool menuOpen = !_log.activeSelf;
         _log.SetActive(menuOpen);
 
-        GameSettings.Instance.isWorldStopped = menuOpen;
+        _gameSettings.isWorldStopped = menuOpen;
 
         Cursor.lockState = menuOpen ? CursorLockMode.None:CursorLockMode.Locked;
         Cursor.visible = menuOpen;
