25b680ce774f50ea6dec8624f44fe52b0b418195
a22007432 <ssm.antonio@gmail.com>
2023-08-10 13:40:37 +0100

    O martinho ajudou homie demais

--- Assets/Scripts/Stylization/CustomPostProcessRenderFeature.cs
+++ Assets/Scripts/Stylization/CustomPostProcessRenderFeature.cs
@@ -6,8 +6,14 @@ using UnityEngine.Rendering.Universal;
 [System.Serializable]
 public class CustomPostProcessRenderFeature : ScriptableRendererFeature
 {
-    private CustomPostProcessPass m_customPass;
+    [SerializeField] 
+    private Shader m_bloomShader;
+    [SerializeField]
+    private Shader m_compositeShader;
 
+
+    private CustomPostProcessPass m_customPass;
+     
     //include other passes here
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
