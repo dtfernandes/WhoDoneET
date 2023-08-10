b77314b8417253086f73c3cbf3b59e2d69b7cede
a22007432 <ssm.antonio@gmail.com>
2023-08-10 16:23:08 +0100

    idk stuff

---
+++ Assets/Scripts/Stylization/PostProcessBenDayDots.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+[PostProcess]
+public class PostProcessBenDayDots : PostProcessEffectSettings
+
+{ }
+
