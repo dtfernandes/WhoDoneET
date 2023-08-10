3412922c6902d5a6fd03f08ff91e0052646b400f
a22007432 <ssm.antonio@gmail.com>
2023-08-10 14:09:07 +0100

    ~just messing around with shaders

--- Assets/Scripts/Stylization/CustomPostProcessRenderFeature.cs
+++ Assets/Scripts/Stylization/CustomPostProcessRenderFeature.cs
@@ -13,16 +13,17 @@ public class CustomPostProcessRenderFeature : ScriptableRendererFeature
 
 
     private CustomPostProcessPass m_customPass;
-     
+         public override void Create()
+    {
+        m_customPass = new CustomPostProcessPass();
+        
+    }
+
     //include other passes here
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
         renderer.EnqueuePass(m_customPass);
     }
 
-    public override void Create()
-    {
-        m_customPass = new CustomPostProcessPass();
-        
-    }
+
 }
