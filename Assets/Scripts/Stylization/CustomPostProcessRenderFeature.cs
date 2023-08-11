1c1923fe033f31619a6070538595d32913af6ea7
a22007432 <ssm.antonio@gmail.com>
2023-08-11 16:02:57 +0100

    this fucking tutorial man

--- Assets/Scripts/Stylization/CustomPostProcessRenderFeature.cs
+++ Assets/Scripts/Stylization/CustomPostProcessRenderFeature.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 [System.Serializable]
@@ -11,19 +12,44 @@ public class CustomPostProcessRenderFeature : ScriptableRendererFeature
     [SerializeField]
     private Shader m_compositeShader;
 
+    private Material m_bloomMaterial;
+    private Material m_compositeMaterial;
 
     private CustomPostProcessPass m_customPass;
-         public override void Create()
+
+    
+
+    public override void SetupRenderPasses(ScriptableRenderer renderer, in RenderingData renderingData)
     {
-        m_customPass = new CustomPostProcessPass();
-        
+        if (renderingData.cameraData.cameraType == CameraType.Game)
+        {
+            m_customPass.ConfigureInput(ScriptableRenderPassInput.Depth);
+            m_customPass.ConfigureInput(ScriptableRenderPassInput.Color);
+            m_customPass.SetTarget(renderer.cameraColorTargetHandle, renderer.cameraColorTargetHandle); 
+        }
     }
 
-    //include other passes here
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
         renderer.EnqueuePass(m_customPass);
     }
 
+
+    public override void Create()
+    {
+        m_bloomMaterial = CoreUtils.CreateEngineMaterial(m_bloomShader);
+        m_compositeMaterial = CoreUtils.CreateEngineMaterial(m_compositeShader);
+         
+        m_customPass = new CustomPostProcessPass(m_bloomMaterial, m_compositeMaterial);
+        
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        CoreUtils.Destroy(m_bloomMaterial);
+        CoreUtils.Destroy(m_compositeMaterial);
+
+        //base.Dispose(disposing);
+    }
 
 }  
