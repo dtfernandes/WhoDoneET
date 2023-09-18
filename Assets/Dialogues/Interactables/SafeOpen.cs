b51b8413123acf00b1a392cf96b7639ff5ac88fc
a22007432 <ssm.antonio@gmail.com>
2023-09-18 13:53:27 +0100

    funky merge

---
+++ Assets/Dialogues/Interactables/SafeOpen.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDialogueEvent : MonoBehaviour
+{
+    public void SafeOpeningd()
+    {
+        Debug.Log("Safeopen!!");
+    }
+}
