39d51934fe9e8bf214a654f34c131bf10cd7cd5f
a22007432 <ssm.antonio@gmail.com>
2023-09-18 23:02:28 +0100

    dia

--- Assets/Dialogues/Interactables/SafeOpen.cs
+++ Assets/Dialogues/Interactables/SafeOpen.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestDialogueEvent : MonoBehaviour
+public class SafeOpenEvent : MonoBehaviour
 {
-    public void SafeOpeningd()
+    public void SafeOpening()
     {
         Debug.Log("Safeopen!!");
     }
