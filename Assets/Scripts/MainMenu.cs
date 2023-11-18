02d8f0979325731dc2f2fb967abdb0aa321694bb
dtfernandes <danielteotonio@sapo.pt>
2023-11-18 10:20:15 +0000

    Fix transition between MainMenu, tutorial and game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,9 +1,29 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private EventSystem eSysteM;
+    [SerializeField]
+    private GameObject Yes;
+
+    public void OpenMenu()
+    {
+        eSysteM.SetSelectedGameObject(null);
+        eSysteM.SetSelectedGameObject(Yes);
+        Debug.Log("hm");
+    }
+
     public void GoToGame()
+    {
+
+        SceneManager.LoadScene("TestTiles 2");
+    }
+
+    public void GoToTutorial()
     {
         SceneManager.LoadScene("Tutorial");
     }
